cbcf10657090980362ac77ae73d2de5692d186ad
Ilya <ilya.kritski2017@gmail.com>
2022-09-12 19:15:26 +0300

    Choice triangle
    Вводим стороны треугольника. Если сумма двух любых сторон меньше длинны третьей, то треугольник не существует. Иначе считаем площадь.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,5 +11,21 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Выберите фигуру(треугольник,круг,квадрат)");
         String figura = in.nextLine();
+        switch (figura) {
+            case "t": {
+                System.out.println("Введите стороны треугольника");
+                int a = in.nextInt();
+                int b = in.nextInt();
+                int c = in.nextInt();
+                if (a + b < c || a + c < b || b + c < a) {
+                    System.out.println("Треугольник не может существовать!");
+                } else {
+                    p = (a + b + c) / 2;
+                    s = (float) Math.sqrt(p * (p - a) * (p - b) * (p - c));
+                    System.out.println("Площадь треугольника:" + s);
+                }
+                break;
+            }
+        }
     }
 }
