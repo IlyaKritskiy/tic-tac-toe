a710a58366034df6f1f2b7d0f6214a8f182c51d2
Ilya <ilya.kritski2017@gmail.com>
2022-09-12 19:07:02 +0300

    Input figure

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        float s;
+        int p;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Выберите фигуру(треугольник,круг,квадрат)");
+        String figura = in.nextLine();
     }
 }
